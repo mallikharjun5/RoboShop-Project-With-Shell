b43927f2db866a31c76088c3638ab22a0d907336
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-07 12:48:22 +0530

    dynamic instances

--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-090908868bc302a71"      # REPLACE WITH YOUR SECURITY GROUP ID
+SG_ID="sg-090908868bc302a71"     # REPLACE WITH YOUR SECURITY GROUP ID
 
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids
     $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances
     [0].InstanceId' --output text)
-
     # GET PRIVATE IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].
