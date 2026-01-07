d80096acd543c3c996a4da72afed1d7dd75ad29b
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-07 18:14:17 +0530

    r53 record update

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-090908868bc302a71"     # REPLACE WITH YOUR SECURITY GROUP ID
+ZONE_ID="Z01508593JUYY9SHIBG2D"  # REPLACE WITH YOUR HOSTED ZONE ID
+DOMAIN="mallikharjundevops.fun"
 
 for instance in $@
 do
@@ -10,10 +12,31 @@ do
     # GET PRIVATE IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN"             # mongodb.mallikharjundevops.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+                RECORD_NAME="$instance.$DOMAIN"     # mallikharjundevops.fun
+
     fi
 
     echo "$instance: $IP"
 
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
