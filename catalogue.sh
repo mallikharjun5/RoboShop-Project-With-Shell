560ee6ca9022c1a39ea7f95e422b77d60ec9accd
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-09 17:20:18 +0530

    jan 9 catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -72,9 +72,20 @@ VALIDATE $? "Enabling Catalogue"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copy Mongo Repo"
+
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB client"
+
 mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 VALIDATE $? "Load Catalogue Products"
-systemctl restart catalogue.service
-VALIDATE $? "Restarted Catalogue"
+
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue products"
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarted catalogue"
